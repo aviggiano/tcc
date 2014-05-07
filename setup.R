633ce2befd5c8debd6850e0f95f7b4f850f67fc6
aviggiano <antonio.viggiano@usp.br>
2014-05-07 11:54:39 -0300

    color map

--- setup.R
+++ setup.R
@@ -4,6 +4,7 @@ items_filename <- "items.csv"
 customer_filename <- "customer.csv"
 brand_filename <- "brand.csv"
 color_filename <- "color.csv"
+color_map_filename <- "color_map.csv"
 
 h1 <- read.csv(history1_filename, as.is=TRUE)
 h2 <- read.csv(history2_filename, as.is=TRUE)
@@ -11,6 +12,7 @@ items <- read.csv(items_filename, as.is=TRUE)
 customers <- read.csv(customer_filename, as.is=TRUE)
 brand <- read.csv(brand_filename, as.is=TRUE)
 color <- read.csv(color_filename, as.is=TRUE)
+color_map <- read.csv(color_map_filename, as.is=TRUE)
 
 colnames(h2) <- colnames(h1)
 history <- rbind(h1,h2)
