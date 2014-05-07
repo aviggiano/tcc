c80fae135676444002b569750e7559dfcd570863
aviggiano <antonio.viggiano@usp.br>
2014-05-07 10:55:45 -0300

    distancia colors

--- functions.R
+++ functions.R
@@ -61,3 +61,31 @@ sim_delta_bag_of_words <- function(xs, ys){
         sum(m[row(m)!=col(m)]) / max(length(xs), length(ys))
     }
 }
+
+sq_dist_L2 <- function(X,Y){
+    ans <- 0
+    for(i in 1:length(X)){
+        ans <- ans + (X[i]-Y[i])*(X[i]-Y[i])
+    }
+    ans
+}
+
+dist_L2 <- function(X,Y){
+    sqrt(sq_dist_L2(X,Y))
+}
+
+dist <- function(X,Y){
+    dist_L2(X,Y)
+}
+
+red <- c(255,0,0)
+green <- c(0,255,0)
+blue <- c(0,0,255)
+black <- c(0,0,0)
+white <- c(255,255,255)
+
+max_dist_color <- dist(black,white)
+
+dc <- function(color1, color2){
+    dist(color1, color2) / max_dist_color
+}
