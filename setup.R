258aa9cc3023cb41ce8c5be6ff1881ee1fa5942e
aviggiano <antonio.viggiano@usp.br>
2014-05-18 16:40:19 -0300

    retirando s dos arquivos

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 history1_filename <- "history1.csv"
 history2_filename <- "history2.csv"
-items_filename <- "items.csv"
+item_filename <- "item.csv"
 customer_filename <- "customer.csv"
 brand_filename <- "brand.csv"
 color_filename <- "color.csv"
@@ -8,8 +8,8 @@ color_map_filename <- "color_map.csv"
 
 h1 <- read.csv(history1_filename, as.is=TRUE)
 h2 <- read.csv(history2_filename, as.is=TRUE)
-items <- read.csv(items_filename, as.is=TRUE)
-customers <- read.csv(customer_filename, as.is=TRUE)
+item <- read.csv(item_filename, as.is=TRUE)
+customer <- read.csv(customer_filename, as.is=TRUE)
 brand <- read.csv(brand_filename, as.is=TRUE)
 color <- read.csv(color_filename, as.is=TRUE)
 color_map <- read.csv(color_map_filename, as.is=TRUE)
