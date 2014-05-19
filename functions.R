f1ffd3613d5b006821225ff810bbfa238d30e42d
aviggiano <antonio.viggiano@usp.br>
2014-05-19 01:41:59 -0300

    similaridade completa... falta melhorar

--- functions.R
+++ functions.R
@@ -48,7 +48,7 @@ dk <- function(x,y){
     delta(x,y)
 }
 
-sim_delta <- function(xs, ys){
+delta_vector <- function(xs, ys){
     ans <- 0
     for(i in 1:length(xs)){
         ans = ans + delta(xs[i], ys[i])
@@ -60,8 +60,8 @@ sim_delta_bag_of_words <- function(xs, ys){
     if (length(xs) == 0 || length(ys) == 0)
         0
     else {
-        m <- sapply(x, function(i){
-            sapply(y, function(j){
+        m <- sapply(xs, function(i){
+            sapply(ys, function(j){
                 delta(i,j)
             })
         })
@@ -98,8 +98,66 @@ dc <- function(color1, color2){
     dist(color1, color2) / max_dist_color
 }    
 
+rgb <- function(color){
+    color_rgb<- color_map[which(color_map['color']==tolower(color)),]
+    c(color_rgb$r, color_rgb$g, color_rgb$b)
+}
+
+sim_color <- function(color1, color2){
+    if(length(rgb(color1)) == 0 || length(rgb(color2)) == 0)
+        0
+    else
+        sim_color_rgb(rgb(color1), rgb(color2))
+}
+
+sim_color_rgb <- function(color1rgb, color2rgb){
+    1-dc(color1rgb, color2rgb)
+}
+
 max_dist_price <- dist(min(wheren('sale_price',0)$sale_price), max(item$sale_price))
 
-dist_price <- function(x, y){
-    dist(x$sale_price, y$sale_price) / max_dist_price
+dist_price <- function(price1, price2){
+    dist(price1, price2) / max_dist_price
+}
+
+sim_price <- function(price1, price2){
+    1 - dist_price(price1, price2)
+}
+
+dist_gender <- function(x,y){
+    masc_fem <- c("Masculino", "Feminino")
+    if (x == y)
+        0
+    else if (x %in% masc_fem && y %in% masc_fem) #um e' masc e o outro e' fem
+        1
+    else
+        0.5 # falta completar
+}
+
+sim_gender <- function(x,y){
+    1-dist_gender(x,y)
+}
+
+sim <- function(i, j){
+    sim_general_item <- delta(i$general_item_id,j$general_item_id)
+    sim_brand <- delta(i$brand, j$brand)
+    sim_attribute_set <- delta(i$attribute_set, j$attribute_set)
+    sim_type <- delta(i$type, j$type)
+    sim_sport <- delta(i$sport, j$sport)
+    sim_category <- delta(i$category, j$category)
+    sim_subcategory <- delta(i$subcategory, j$subcategory)
+    sim_gender <- sim_gender(i$gender, j$gender)
+    sim_color <- sim_color(i$color, j$color)
+    sim_sale_price <- sim_price(i$sale_price, j$sale_price)
+    sim <- c(sim_general_item,
+             sim_brand,
+             sim_attribute_set,
+             sim_type,
+             sim_sport,
+             sim_category,
+             sim_subcategory,
+             sim_gender,
+             sim_color,
+             sim_sale_price)
+    sum(sim) / length(sim)       
 }
