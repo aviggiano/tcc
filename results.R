a901e44ae349661376973b6c11d5f74d919819d9
aviggiano <antonio.viggiano@usp.br>
2014-06-24 12:02:48 -0300

    results

--- results.R
+++ results.R
@@ -4,15 +4,24 @@ fw1_stats <- function() {
     m
 }
 
-plot_fm1 <- function() {
-    ggplot(m, aes(x=m$I, y=m$T))
-    +geom_point(size=3)
-    +stat_smooth(method="lm")
-    +scale_y_log10(labels=fmt(), breaks=c(m$T[1],m$T[2],m$T[3],m$T[4]))
-    +scale_x_log10(breaks=c(1E0,1E1,1E2,1E3))
-    +xlab("Quantidade de itens")
-    + ylab("Tempo de processamento (s)")
-
-    ggsave(file="latex/beamer/img/ixt.png")
-
+fmt <- function(){
+    f <- function(x) as.character(round(x,2))
+    f
 }
+
+plot_fm1 <- function() {
+    library(ggplot2)
+    m <- fw1_stats()
+    
+    ggplot(m, aes(x=m$I, y=m$T)) +
+        geom_point(size=3) +
+            stat_smooth(method="lm") +
+                scale_y_log10(labels=fmt(), breaks=c(m$T[1],m$T[2],m$T[3],m$T[4])) +
+                    scale_x_log10(breaks=c(1E0,1E1,1E2,1E3)) +
+                        xlab("Quantidade de itens") +
+                            ylab("Tempo de processamento (s)")
+    
+    ggsave(file="latex/beamer/img/ixt.png")
+}
+
+
