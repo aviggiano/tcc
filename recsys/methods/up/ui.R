24a3b374ec17726c0733a598028a5c9a3f15d668
aviggiano <antonio.viggiano@usp.br>
2014-09-06 22:13:11 -0300

    verificar por que UP nao esta dando bons resultados... nao ignorar FW e FW1 s

--- recsys/methods/up/ui.R
+++ recsys/methods/up/ui.R
@@ -10,11 +10,11 @@ get_omega_ui = function(w, a, debug){
   omega
 }
 
-ui = function(a, r, U, M=2, k=2, debug=FALSE){
+ui = function(a, r, U, M=2, k=2, N=1, debug=FALSE){
   t = get_t(a, r, U, M, debug)
   q = get_q(t, U, debug)
   w = get_w(t, q, debug)
   omega = get_omega_ui(w, a, debug)
-  iu = get_iu(omega, r, U, k, debug)
+  iu = get_iu(omega, r, U, N, debug)
   iu
 }
--- recsys/results/performance.R
+++ recsys/results/performance.R
@@ -1,4 +1,5 @@
 source('recsys/methods/up/up.R')
+source('recsys/methods/up/ui.R')
 
 hide.data = function(R, kappa = 0.75){
   R.length = length(R)
@@ -7,7 +8,7 @@ hide.data = function(R, kappa = 0.75){
   R
 }
 
-performance.up = function(a, r, U, M=2, k=2, N=10, debug=FALSE){
+performance = function(a, r, U, M=2, k=2, N=10, debug=FALSE){
   rtest = hide.data(r)
   if(debug){
     print("h(r)")
@@ -15,8 +16,14 @@ performance.up = function(a, r, U, M=2, k=2, N=10, debug=FALSE){
     print("h(rtest)")
     print(h(rtest))
   }
+  up = performance.up(a, r, rtest, U, M, k, N, debug)
+  ui = performance.ui(a, r, rtest, U, M, k, N, debug)
+  print(up)
+  print(ui)
+}
+
+performance.up = function(a, r, rtest, U, M=2, k=2, N=10, debug=FALSE){
   iu = up(a, rtest, U, M, k, N, debug)
-  print(iu)
   TP = sum(sapply(1:length(U), function(u) length(intersect(iu[[u]], which(r[u,]>M)))))
   P = list.length(iu)
   RELEVANT = length(which(rtest>2)) #sum(sapply(1:length(U), function(u) length(which(rtest[u,]>M))))
@@ -32,4 +39,23 @@ performance.up = function(a, r, U, M=2, k=2, N=10, debug=FALSE){
     print(F1)
   }
   list(precision, recall, F1)
+}
+
+performance.ui =  function(a, r, rtest, U, M=2, k=2, N=10, debug=FALSE){
+  iu = ui(a, rtest, U, M, k, N, debug)
+  TP = sum(sapply(1:length(U), function(u) length(intersect(iu[[u]], which(r[u,]>M)))))
+  P = list.length(iu)
+  RELEVANT = length(which(rtest>2)) #sum(sapply(1:length(U), function(u) length(which(rtest[u,]>M))))
+  precision = TP/P
+  recall = TP/RELEVANT
+  F1 = 2 * (precision * recall) / (precision + recall)
+  if(debug){
+    print("precision")
+    print(precision)
+    print("recall")
+    print(recall)
+    print("F1")
+    print(F1)
+  }
+  list(precision, recall, F1)  
 }
