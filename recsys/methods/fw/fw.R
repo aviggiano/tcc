d031e271072371807520ce4757efa630a19cd4dc
aviggiano <antonio.viggiano@usp.br>
2014-09-18 11:34:37 -0300

    fixed generic method for lm.W

--- recsys/methods/fw/fw.R
+++ recsys/methods/fw/fw.R
@@ -1,34 +1,54 @@
 source('recsys/methods/fw/fw_dij.R')
 
-get_W = function(d1, d2, r, debug){
-  ones = matrix(1,length(r[1,]), length(r[1,]))
-  # best fit to W = [w0, w_release_date, w_gender]'
-  # Y = A X
-  # Y = as.vector(e)
-  # A = ONES D1 D2
-  
-  # equation not valid for i=j
-  diag(d1) = NA
-  diag(d2) = NA
-  diag(ones) = NA
-  D1 = cbind(as.vector(1-d1))
-  D2 = cbind(as.vector(1-d2))
-  ONES = cbind(as.vector(ones))
-  Y = cbind(as.vector(e))
-  
-  A = cbind(ONES, D1, D2, deparse.level = 0)
-  # maneira idiota de remover da equação os elementos i=j
-  IequalsJ = which(is.na(A))
-  A=A[-IequalsJ,]
-  Y=cbind(Y[-IequalsJ])
-  
-  # resolve o sistema de equações
-  W = solve(t(A) %*% A) %*% t(A) %*% Y
-  if(debug){
-    print("W")
-    print(W)
+get_W = function(d1, d2, r, debug, generic){
+  if(!generic){
+    ones = matrix(1,length(r[1,]), length(r[1,]))
+    # best fit to W = [w0, w_release_date, w_gender]'
+    # Y = A X
+    # Y = as.vector(e)
+    # A = ONES D1 D2
+    
+    # equation not valid for i=j
+    diag(d1) = NA
+    diag(d2) = NA
+    diag(ones) = NA
+    D1 = cbind(as.vector(1-d1))
+    D2 = cbind(as.vector(1-d2))
+    ONES = cbind(as.vector(ones))
+    Y = cbind(as.vector(e))
+    
+    A = cbind(ONES, D1, D2, deparse.level = 0)
+    # removing elements i=j
+    IequalsJ = which(is.na(A))
+    A=A[-IequalsJ,]
+    Y=cbind(Y[-IequalsJ])
+    
+    # resolve o sistema de equações
+    W = solve(t(A) %*% A) %*% t(A) %*% Y
+    if(debug){
+      print("W")
+      print(W)
+    }
+    W
   }
-  W
+  else {
+    D = matrix(0, length(d[,,1]), length(d[1,1,]))
+    for(f in 1:length(d[1,1,])){
+      diag(d[,,f]) = NA
+      D[,f] = as.vector(1-d[,,f])
+    }
+    
+    # removing elements i=j
+    IequalsJ = which(is.na(D))
+    D=D[-IequalsJ,]
+    E = cbind(as.vector(e))
+    E=cbind(E[-IequalsJ])
+    
+    # linear fit E ~ w0 + w D
+    lm.W = lm(E ~ D)
+    lm.W
+  }
+  
 }
 
 get_s = function(W, d1, d2, debug){
