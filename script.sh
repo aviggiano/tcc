f6a200a88f80e138f0afa179a805f6d023359881
aviggiano <antonio.viggiano@usp.br>
2014-11-05 15:24:33 -0200

    yes script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo su
-yum install R 				# instala o pacote R no linux
-yum install git 			# instala o git para carregar os métodos de recomendação
+yes | yum install R 				# instala o pacote R no linux
+yes | yum install git 			# instala o git para carregar os métodos de recomendação
 ssh-keygen -t rsa 		# gera a chave publica para se poder carregar o repositorio do sistema
 cat ~/.ssh/id_rsa.pub	# depois, deve-se adicionar essa chave publica nas configurações do Github
 git clone git@github.com:aviggiano/tcc	# clona o repositorio
