3c105e5a9eb9a70ba2a867d02ccffb84beb68c82
aviggiano <antonio.viggiano@usp.br>
2014-11-05 05:31:18 -0200

    script sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+yum install R 				# instala o pacote R no linux
+yum install git 			# instala o git para carregar os métodos de recomendação
+ssh-keygen -t rsa 		# gera a chave publica para se poder carregar o repositorio do sistema
+cat ~/.ssh/id_rsa.pub	# depois, deve-se adicionar essa chave publica nas configurações do Github
+git clone git@github.com:aviggiano/tcc	# clona o repositorio
+cd tcc && Rscript recsys/run_tests.R 		# executa o script de avaliacao
